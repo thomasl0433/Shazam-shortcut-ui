6f50f8fd12169f7f0e2d9d307b07989cd3c6fd0a
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2023-01-02 11:38:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
+  const [token, setToken] = useState("");
+  const [expired, setExpired] = useState("");
+
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_ENV === "DEV" ? "http://localhost:3000" : "https://spotify-subgenre-thomasl0433.vercel.app";
+  const RESPONSE_TYPE = "token";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+
+  const loginString = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+
+  useEffect(() => {
+    // cache token
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+    setToken(token);
+    sendToExpress(token);
+  }, [])
+
+  const logout = () => {
+    setToken("");
+    setExpired(null)
+    window.localStorage.removeItem("token");
+  }
+
+  const sendToExpress = async (token) => {
+    // prod url ---> https://shazam-shortcut-pqymwrjaca-uc.a.run.app
+    await axios.get("http://localhost:8888/receive-token", {
+      params: {
+        data: token
+      }
+    }).then((res) => {
+      console.log(`Response status: ${res.status}`)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container flex flex-col mx-auto items-center p-8">
+      <h1 className="text-3xl font-bold mb-8 py-4">Shazam Shortcut Login Portal</h1>
+      { !token && !expired ?
+        <a href={loginString} className="rounded-full p-3 text-gray-100 bg-spotify-green">Login to Spotify</a> 
+        : <button className="rounded-full p-3 text-gray-100 bg-spotify-green" onClick={logout}>Logout</button>
+      }
+
+      <div className='lg:m-10 m-4 lg:w-1/3'>
+        <div className="bg-gray-200 rounded-xl p-6 mb-5 shadow-lg">
+                <h3 className="font-bold text-gray-700 mb-2">🚨 Note:</h3>
+                <p className="text-gray-500">To use the Shazam shortcut, you must login to Spotify here which will allow you to use the tool as many times as you want for ONE hour ⏳
+                </p>
+            </div>
+      </div>
+      
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'spotify-green': '#1DB954',
+        'link-color': '#0000EE'
+      }
+    },
+  },
+  plugins: [],
+}
